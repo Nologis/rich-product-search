dd157c0d5a86e6b3df051f251d76401a46b6ac2a
raulanatol <raul@natol.es>
2018-04-03 11:51:33 +0100

    Configuration files

---
+++ test/containers/RichProductSearch.test.tsx
@@ -0,0 +1,10 @@
+import { shallow } from 'enzyme';
+import * as React from 'react';
+import RichProductSearch from '../../src/containers/RichProductSearch';
+
+describe('RichProductSearch', () => {
+  it('Should render the RichProductSearch', () => {
+    const component = shallow(<RichProductSearch apiKey={'123'}/>);
+    expect(component.find('h1')).toHaveLength(1);
+  });
+});
--- test/index.test.ts
+++
@@ -1,8 +0,0 @@
-import { init } from '../src';
-
-describe('Index', () => {
-  it('Should render the RichProductSearch if exists component and apiKey in the document', () => {
-    init();
-    expect(window.nologis.richProductSearch).toBeDefined();
-  });
-});
